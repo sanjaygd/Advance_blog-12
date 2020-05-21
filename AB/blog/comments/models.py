f8b474fc76d271bc1a7a500ff0bd9d6a2ed1c994
sanjaygd <sanjaygd96@gmail.com>
2020-05-21 13:09:21 +0530

    comments added in models.py

--- AB/blog/comments/models.py
+++ AB/blog/comments/models.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey # This is added in generic foriegnkey part-2
 from django.contrib.contenttypes.models import ContentType
 
 from django.db import models
@@ -7,11 +7,12 @@ from django.db import models
 from blog_app.models import Post
 
 
-# Added in generic foriegn key part1
+
 class Comments(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
     # post = models.ForeignKey(Post, on_delete=models.CASCADE) 
 
+    # This is added in generic foriegnkey part-2
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
